e9907edcffd2c363459b7cba9e7a933a1b35942a
BasilisAndr <blindedbysunshine@gmail.com>
2019-02-28 22:02:51 +0300

    graph stuff

--- input.py
+++ input.py
@@ -17,7 +17,8 @@ def add_node(G, tags, orient):
 
 
 def add_edge(G, a, b):
-    G.add_edge(hash(a), hash(b), weight=0-count_interest(a, b))
+    G.add_edge(hash(a[0]), hash(b[0]), weight=0-count_interest(a[1], b[1]))
+    return
 
 
 def make_graph(photos):
@@ -27,6 +28,15 @@ def make_graph(photos):
     for tags in photos['v']:
         add_node(G, tags, 'v')
     return G
+
+
+def get_sorted_edges(G):
+    edges = []
+    nodes = list(G.nodes(data=True))
+    for x in range(len(nodes)):
+        for y in range(x, len(nodes)):
+            edges.append((nodes[x], nodes[y], count_interest(nodes[x][1], nodes[y][1])))
+    return sorted(edges, key=lambda x: x[2], reverse=True)
 
 
 def main():
@@ -40,7 +50,7 @@ def main():
         else:
             photos['v'].append(tags)
     G = make_graph(photos)
-    print(G.nodes(data=True))
+    print(get_sorted_edges(G))
     # print("Case #{}: {}".format(i, find_solution(n, p, tags)))
 
 main()
